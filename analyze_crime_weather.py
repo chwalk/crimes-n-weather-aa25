6a2256c140737a1ab0eaaf15fb914ab1b334c860
carywalk1 <carywalk1@gmail.com>
2025-03-21 17:17:13 -0500

    Removed debug code. Added year support.
    I removed my earlier debugging code. I added support for a user to specify a single year or "ALL". The code all needs commenting.

--- analyze_crime_weather.py
+++ analyze_crime_weather.py
@@ -2,24 +2,21 @@ import pandas as pd
 import sklearn.linear_model as lm
 
 
-def main():
-    crime_weather_df = pd.read_csv("C:/Users/chwalker/Downloads/merged_file_weather+daily_counts.csv")
-    # print("Displaying Raw Crime Weather")
-    # print(crime_weather_df)
-    # print("Done Displaying Raw Crime Weather")
+YEAR = "ALL"
+
+
+def main(year):
+    crime_weather_df = pd.read_csv("C:/Users/walkadmin/Downloads/merged_file_weather+daily_counts.csv", parse_dates=['Date'])
+
+    if year != "ALL":
+        if type(year) == str:
+            year = int(year)
+        crime_weather_df = crime_weather_df[crime_weather_df.Date.dt.year == year]
 
     crime_weather_df = crime_weather_df.dropna()
-    # print("Displaying Cleaned")
-    # print(cleaned_crime_weather_df)
-    # print("Done displaying cleaned.")
     
-    # print(crime_weather_df.columns)
     number_of_crimes_df = crime_weather_df[['NumberofCrimes']]
     temp_df = crime_weather_df[['Temp']]
-    
-    # cleaned_temp_df = temp_df.dropna()
-    # print(cleaned_temp_df)
-    # print(temp_df)
 
     model = lm.LinearRegression()
     model.fit(temp_df, number_of_crimes_df)
@@ -28,4 +25,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(YEAR)
